92f9abb69f16a154951ca4e6bd6c45618253146a
Alex <22248105+Parametricall@users.noreply.github.com>
2018-12-23 14:38:06 +1000

    Images are now displayed with dinner options.

--- app/src/main/java/com/parametricall/dinner/DinnerOption.java
+++ app/src/main/java/com/parametricall/dinner/DinnerOption.java
@@ -2,6 +2,8 @@ package com.parametricall.dinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -10,6 +12,8 @@ public class DinnerOption extends AppCompatActivity {
 
     Database myDb;
     TextView dinnerOptionName, dinnerOptionIngredients;
+    ImageView dinnerOptionImage;
+    Integer tableID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,77 @@ public class DinnerOption extends AppCompatActivity {
         myDb = new Database(getApplicationContext());
         dinnerOptionName = findViewById(R.id.dinnerOptionNameTextView);
         dinnerOptionIngredients = findViewById(R.id.dinnerOptionIngredientsTextView);
+        dinnerOptionImage = findViewById(R.id.dinnerOptionImageView);
+
+        Integer initialDbSize = myDb.getData().size();
 
         ArrayList<String[]> data = myDb.getData();
-        String firstName = data.get(0)[0];
-        String Ingredient = data.get(0)[1];
+        if (!data.isEmpty()) {
+            String firstName = data.get(tableID)[0];
+            String Ingredient = data.get(tableID)[1];
 
-        dinnerOptionName.setText(firstName);
-        dinnerOptionIngredients.setText(Ingredient);
+            dinnerOptionName.setText(firstName);
+            dinnerOptionIngredients.setText(Ingredient);
+            dinnerOptionImage.setImageResource(R.drawable.steak_and_chips_edit);
+        } else {
+            dinnerOptionName.setText(getString(R.string.empty_database));
+        }
+    }
 
+    public void onClickNextDinner(View view) {
+        tableID++;
+
+        ArrayList<String[]> data = myDb.getData();
+
+        if (tableID >= data.size()) {
+            tableID = 0;
+        }
+        if (!data.isEmpty() && data.size() > 1) {
+            String firstName = data.get(tableID)[0];
+            String Ingredient = data.get(tableID)[1];
+
+            dinnerOptionName.setText(firstName);
+            dinnerOptionIngredients.setText(Ingredient);
+
+            if (firstName.equals("Steak and Chips")) {
+                dinnerOptionImage.setImageResource(R.drawable.steak_and_chips_edit);
+            } else if (firstName.equals("Chicken Curry")) {
+                dinnerOptionImage.setImageResource(R.drawable.chicken_curry);
+            } else if (firstName.equals("Fish n Chips")) {
+                dinnerOptionImage.setImageResource(R.drawable.fish_and_chips);
+            } else {
+                dinnerOptionImage.setVisibility(view.INVISIBLE);
+            }
+        } else {
+            dinnerOptionName.setText(getString(R.string.empty_database));
+            dinnerOptionIngredients.setText("");
+        }
+    }
+
+    public void onClickPrevDinner(View view) {
+        tableID--;
+        ArrayList<String[]> data = myDb.getData();
+        if (tableID < 0) {
+            tableID = data.size() - 1;
+        }
+        if (!data.isEmpty()) {
+            String firstName = data.get(tableID)[0];
+            String Ingredient = data.get(tableID)[1];
+
+            dinnerOptionName.setText(firstName);
+            dinnerOptionIngredients.setText(Ingredient);
+            if (firstName.equals("Steak and Chips")) {
+                dinnerOptionImage.setImageResource(R.drawable.steak_and_chips_edit);
+            } else if (firstName.equals("Chicken Curry")) {
+                dinnerOptionImage.setImageResource(R.drawable.chicken_curry);
+            } else if (firstName.equals("Fish n Chips")) {
+                dinnerOptionImage.setImageResource(R.drawable.fish_and_chips);
+            } else {
+                dinnerOptionImage.setVisibility(view.INVISIBLE);
+            }
+        } else {
+            dinnerOptionName.setText(getString(R.string.empty_database));
+            dinnerOptionIngredients.setText("");
+        }
     }
 }
